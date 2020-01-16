05683ef7eaf18d5ffb497b8dcbf5299db4bf3e06
PANSUK <donggov@gmail.com>
2020-01-16 20:54:30 +0900

    User 검색 firebase 연동

--- instagram_clone/lib/screens/search_screen.dart
+++ instagram_clone/lib/screens/search_screen.dart
@@ -1,7 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/models/user_model.dart';
 import 'package:instagram_clone/models/user_repository.dart';
 import 'package:instagram_clone/screens/pofile_screen.dart';
+import 'package:instagram_clone/services/database_service.dart';
 
 class SearchScreen extends StatefulWidget {
   SearchScreen({
@@ -17,18 +20,43 @@ class SearchScreen extends StatefulWidget {
 
 class _SearchScreenState extends State<SearchScreen> {
   TextEditingController _searchController = TextEditingController();
-  List<User> _searchUsers = [];
-
-  @override
-  void initState() {
-    super.initState();
-//    _searchUsers = UserRepository.findAllByName("");
-    _searchController.addListener(() => widget.onTextChanged != null ? widget.onTextChanged(_searchController.text) : null);
+  Future<QuerySnapshot> _users;
+  
+  _buildUserTile(User user) {
+    return ListTile(
+        leading: CircleAvatar(
+          radius: 20.0,
+          backgroundImage: user.profileImageUrl.isEmpty ? AssetImage('assets/images/boy.png') : CachedNetworkImageProvider(user.profileImageUrl)),
+        title: Text(user.name),
+        onTap: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => ProfileScreen(userId: user.id),
+            )
+        )
+    );
   }
 
-  _onChanged(String value) {
+//  List<User> _searchUsers = [];
+
+//  @override
+//  void initState() {
+//    super.initState();
+//    _searchUsers = UserRepository.findAllByName("");
+//    _searchController.addListener(() => widget.onTextChanged != null ? widget.onTextChanged(_searchController.text) : null);
+//  }
+
+//  _onChanged(String value) {
+//    setState(() {
+//      _searchUsers = UserRepository.findAllByName(value);
+//    });
+//  }
+
+  _clearSearch() {
+    print("clear");
+    WidgetsBinding.instance.addPostFrameCallback((_) => _searchController.clear());
     setState(() {
-      _searchUsers = UserRepository.findAllByName(value);
+      _users = null;
     });
   }
 
@@ -49,41 +77,71 @@ class _SearchScreenState extends State<SearchScreen> {
             ),
             suffixIcon: IconButton(
               icon: Icon(Icons.clear),
-              onPressed: () {
-                WidgetsBinding.instance.addPostFrameCallback((_) => _searchController.clear());
-                _onChanged('');
-              }
+//              onPressed: () {
+//                WidgetsBinding.instance.addPostFrameCallback((_) => _searchController.clear());
+//                _onChanged('');
+//              }
+              onPressed: _clearSearch,
             ),
             filled: true,
           ),
-//          onSubmitted: (input) {
-//            print(input);
-//          },
-          onChanged: _onChanged,
+          onSubmitted: (input) {
+            if (input.isNotEmpty) {
+              setState(() {
+                _users = DatabaseService.findAllUsersByName(input);
+              });
+            }
+          },
+//          onChanged: _onChanged,
         ),
       ),
-      body: Column(
-        children: <Widget>[
-          (_searchUsers != null && _searchUsers.length != 0)
-              ? Expanded(
-                  child: ListView(
-                    padding: EdgeInsets.all(10.0),
-                    children: _searchUsers.map((user) {
-                      return ListTile(
-//                          leading: Icon(Icons.map),
-                          leading: CircleAvatar(radius: 20.0, backgroundImage: AssetImage('assets/images/boy.png'),),
-                          title: Text(user.name),
-                          onTap: () => Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (_) => ProfileScreen(userId: user.id),
-                          ))
-                      );
-                    }).toList(),
-                  ),
-                )
-              : SizedBox(),
-        ],
-      )
+      body: _users == null
+          ? Center(
+              child: Text('Search for a user'),
+            )
+          : FutureBuilder(
+              future: _users,
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                if (snapshot.data.documents.length == 0) {
+                  return Center(child: Text('No users found'),);
+                }
+
+                return ListView.builder(
+                    itemCount: snapshot.data.documents.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      User user = User.fromDoc(snapshot.data.documents[index]);
+                      return _buildUserTile(user);
+                    }
+                );
+              },
+            )
+//      body: Column(
+//        children: <Widget>[
+//          (_searchUsers != null && _searchUsers.length != 0)
+//              ? Expanded(
+//                  child: ListView(
+//                    padding: EdgeInsets.all(10.0),
+//                    children: _searchUsers.map((user) {
+//                      return ListTile(
+////                          leading: Icon(Icons.map),
+//                          leading: CircleAvatar(radius: 20.0, backgroundImage: AssetImage('assets/images/boy.png'),),
+//                          title: Text(user.name),
+//                          onTap: () => Navigator.push(
+//                              context,
+//                              MaterialPageRoute(builder: (_) => ProfileScreen(userId: user.id),
+//                              ))
+//                      );
+//                    }).toList(),
+//                )
+//              )
+//              : SizedBox(),
+//        ],
+//      )
     );
   }
 
--- instagram_clone/lib/services/database_service.dart
+++ instagram_clone/lib/services/database_service.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:instagram_clone/models/user_model.dart';
 import 'package:instagram_clone/utilities/constants.dart';
 
@@ -9,6 +10,12 @@ class DatabaseService {
       'profileImageUrl': user.profileImageUrl,
       'bio': user.bio,
     });
+  }
+
+  static Future<QuerySnapshot> findAllUsersByName(String name) {
+    // todo : like 검색 어떻게 하지???
+    Future<QuerySnapshot> users = userRef.where('name', isGreaterThanOrEqualTo: name).getDocuments();
+    return users;
   }
 
 }
